025d5d81c62371eb88020b6ee1a460f7376cc23a
kookoo <koo_g0kek@hotmail.com>
2019-10-20 21:24:05 +0700

    up

---
+++ assingment/src/components/cv.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class CV extends React.Component {
+  render() {
+    return (
+      <div className="container col-md-5">
+        <h1> CV</h1>
+        <h1> Jessada Chantdang </h1>
+        <hr></hr>
+        <h5>9/6 The Tempo Ratchadaphisek Huaykwang, Bangkok 10310</h5>
+        <h5>0816087531 iamg0kek@gmail.com</h5>
+        <hr></hr>
+        <h4>SUMMARY</h4>
+        I am a developer who is an expertise in C#, Basic, SQL.
+        I have gained a lot of experience in development window applications for over 10 years.
+        <br />
+        That's my passion to learn new things in web developer fields and to become a successful developer.
+        <hr></hr>
+        <h4>PERSONAL INFORMATION</h4>
+        Birth date – Jan 4, 1985 Marital Status – Single
+        <hr></hr>
+        <h4>SKILLS</h4>
+        ·	Experienced in ASP .Net Programming for 5 years <br />
+        ·	Strong knowledge of C# <br />        
+        ·	Strong knowledge in SQL Server <br />
+        ·	Have an experience in AngularJs, NodeJs <br />
+        ·	Basic knowledge about Go Lang <br />
+        ·	Industry knowledge in Manufacturing, Inventory System <br />
+        ·	Ability to work as a team, flexible, proactive, well organized and focus on objectives <br />
+        ·	Ability to working under pressure, multitask, prioritize and work under time constraints <br />
+        ·	Good command in English <br />
+        ·	Good interpersonal skill <br />
+
+        <hr></hr>
+        <h4>EDUCATION</h4>
+        Bachelor’s Degree, Computer Engineering, Prince of Songkla University (2003 - 2008)
+        <hr></hr>
+        <h4>WORK EXPERIENCE</h4>
+        Develop various application projects for company's customers <br />
+        2 years in development application for ERP Business <br />
+        1 years in development application for Toshiba Logistics <br />
+        2 years in development Leasing System application for Summit Capital Leasing. <br />
+        3 years in development Personal Loan System application for Summit Capital Leasing. <br />
+      </div>
+    );
+  }
+}
+
+export default CV;
